c50e3fe5a98d654ed385889924191ff73433d9fa
Ignacio Cano <ignaciojcano@gmail.com>
2020-02-16 15:30:40 -0400

    remove kube sh

--- run_kubernetes.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-dockerpath=icano/udacity-capstone
-
-kubectl create deployment udacity-capstone --image=$dockerpath:latest
-
-kubectl get pods
-
-kubectl port-forward deployment/udacity-capstone 8000:80
